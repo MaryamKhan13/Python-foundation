b948180121d5b6254e8030a8d3e9c1c07ed7585d
MaryamKhan13 <mishimk0116@gmail.com>
2026-04-02 20:45:31 +0000

    done for lesson two

--- 02_Story Generator.py
+++ 02_Story Generator.py
@@ -8,3 +8,7 @@ Friend=input("Enter the name of your friend:")
 Teacher=input("Enter a teacher:")
 Adjective=input(" Enter an adjective:")
 Time=input("Enter a time:")
+# Creating a story
+story=f"One day at{time}{name}, went to{Place} to eat{Food} with there{Friend},Next we went to play{Action}, it was a {Adjective} day and i was feeling{Feeling} until, I saw{Teacher} and we started{Verb ending in -ing}."
+print("/n---YOUR ADVENTURE STORY---")
+print(story)

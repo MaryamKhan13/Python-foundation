a1756b3f82dbace003cb49d17119cbf7be101acd
MaryamKhan13 <mishimk0116@gmail.com>
2026-04-02 20:28:34 +0000

    Lesson two

--- 02_Story Generator.py
+++ 02_Story Generator.py
@@ -1,5 +1,5 @@
 # Gathering user data
-name=input("Maryam")
-action=input("Swimming and running")
-place=input("Pool")
-feeling=input("Happy and warm")
+name=input("Enter a name:")
+action=input("Enter an action(verb ending in -ing):")
+place=input("Enter a place:")
+feeling=input("How are you feeling today?")

5c5a067992ca513624a0b0c63927f2bf695337bd
MaryamKhan13 <mishimk0116@gmail.com>
2026-04-07 20:19:38 +0000

    Lesson 4

--- 03_Calculator.py
+++ 03_Calculator.py
@@ -1,6 +1,17 @@
 
-
 num1=int(input("Enter first number:"))
 num2=int(input("Enter second number"))
-result=num1 + num2
-print(f"The total of{num1}and{num2}is:{result}")
+result= num1 + num2
+print(f"The Total of {num1} and {num2} is :{result}")
+num3=int(input("Enter third number:"))
+num4=int(input("Enter fourth number"))
+result= num3 - num4
+print(f"The Diffrence of {num3} and {num4} is :{result}")
+num5=int(input("Enter Fifth number:"))
+num6=int(input("Enter Sixth number"))
+result= num5 * num6
+print(f"The Product of {num5} and {num6} is :{result}")
+num7=int(input("Enter Seventh number:"))
+num8=int(input("Enter Eighth number"))
+result= num7 / num8
+print(f"The Quotient of {num7} and {num8} is :{result}")

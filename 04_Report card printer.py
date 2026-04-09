f538d21498704a96b2f074895cb7749242e61426
MaryamKhan13 <mishimk0116@gmail.com>
2026-04-09 19:51:20 +0000

    Report card

---
+++ 04_Report card printer.py
@@ -0,0 +1,12 @@
+name = 'Maryam'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 13
+print(age, type(age))
+
+score = 96
+print(isinstance(score, float))
+

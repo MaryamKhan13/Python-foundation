81f8465f870b069c939cbe4654c3c321f7004114
MaryamKhan13 <mishimk0116@gmail.com>
2026-04-23 19:48:29 +0000

    Welcom back maryam

---
+++ 010_welcome_tool.py
@@ -0,0 +1,8 @@
+#'def'is short for define. 'name' is the input(parameter)
+def greet_user(user_name):
+ print("-------------------")
+ print(f"Welcome back,{user_name}!")
+print("Your developer dashboard is ready.")
+print("---------------------")
+greet_user("Maryam")
+greet_user("Sam")

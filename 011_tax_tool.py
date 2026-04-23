dd0b3ddcce239cc6ef60e5cb2d874816ae1fda63
MaryamKhan13 <mishimk0116@gmail.com>
2026-04-23 20:03:36 +0000

    Tax tool

---
+++ 011_tax_tool.py
@@ -0,0 +1,9 @@
+
+
+
+def calculate_total(price,tax_rate):
+ tax_amount=price*tax_rate
+ total= price+tax_rate
+ print(f"The final price with tax is: ${total:.2f}")
+calculate_total(67.00,0.54)
+calculate_total(5.00,0.54)

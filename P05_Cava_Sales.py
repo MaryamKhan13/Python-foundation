59ced39e9473b12a8cb6d6d4a898d4fc23fcf685
MaryamKhan13 <mishimk0116@gmail.com>
2026-04-28 20:02:18 +0000

    Save bowl

---
+++ P05_Cava_Sales.py
@@ -0,0 +1,4 @@
+Base=["Brown Basmati rice with Arudula,chicken and no suace", Saffron Basmati rice with super greens,falefel and hummus","Black lentils with baby spinach,grilled beef,pickels, and ranch"]
+prices=[3,2,4]
+num_Bowl=len(Base)
+print(f"We sell many {num_Bowl} diffrent kids of Bowls")

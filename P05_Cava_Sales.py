568637e723521ee02896d70a4f2c903efaa567f7
MaryamKhan13 <mishimk0116@gmail.com>
2026-04-28 20:15:25 +0000

    coah

--- P05_Cava_Sales.py
+++ P05_Cava_Sales.py
@@ -1,4 +1,17 @@
 Base=["Brown Basmati rice with Arudula,chicken and no suace", Saffron Basmati rice with super greens,falefel and hummus","Black lentils with baby spinach,grilled beef,pickels, and ranch"]
-prices=[3,2,4]
+prices=[9,12,14]
 num_Bowl=len(Base)
 print(f"We sell many {num_Bowl} diffrent kids of Bowls")
+Bowls=list(zip(prices,toppings))
+print("\nUnsorted Bowls(price,Bowls):")
+print(Bowls)
+Bowls.sort() #SORTS FROM CEAPEST TO MOST EXPENSIVE
+print("\nSorted Menu:")
+print(Bowls)
+cheapest_Bowl=Bowl[0]
+priciest_Bowl=Bowl[2]
+three_cheapest=Bowl[:3] #Slices from index 0 to 2
+print(f"\nCheapest option: {cheapest_Bowl}")
+print(f"Priciest option: {priciest_Bowl}")
+print(f"Budget Options: {three_cheapest}")
+

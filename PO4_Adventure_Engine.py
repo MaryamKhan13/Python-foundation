dfb21a91fd0927520575c8543af715d32bff0486
MaryamKhan13 <mishimk0116@gmail.com>
2026-04-28 19:42:37 +0000

    It works

--- PO4_Adventure_Engine.py
+++ PO4_Adventure_Engine.py
@@ -9,19 +9,43 @@ def start_room():
   print(" I think you misunderstood me. I have a stomach bug AND YOU KNOW THAT.")  
  return "end"
 def examine_room():
-  print("n\---THE LAST PHASE---")
-print("YOUR PATIENT’S SYMPTOMS ARE CLEARLY A SIGN OF A STOMACH BUG. YOU HAVE ONE CHANCE FOR A DIAGNOSIS TO LIVE. CHOOSE THE BEST SOLUTION.")
-choice=input("Here is your diagnsis[1] Prochlorperazine or [2] Just drink water")
-if choice=="1":
- print(" Thank you so much. THE PATIENT LEFT HAPPILY AND YOU STAYED ALIVE BECUASE OF THE GREAT FEEDBACK SHE GAVE. CONGRATS YOU WIN!")
-return "end"
-else:
-print(" You did not listen to me at all. THE PATIENT LEFT ANGERLIY AND LATER RETURNED DUE TO HER DIESIES INCREASING BECUASE SHE REFUSED THE MEDICATION. YOU THEN GOT FIRED AND DIED THE SAME WEEK DUE TO YOUR FAILED JOB. YOU LOSE")  
-return "end"
-current_room="start"
-print("Welcome to docter simulater")
-while current_room!="end":
-    if current_room=="satrt":
-       current_room=start_room() 
-    elif current_room=="examine_room":
-     current_room=tresure_room()
+    print("\n---THE LAST PHASE---")
+    print("YOUR PATIENT’S SYMPTOMS ARE CLEARLY A SIGN OF A STOMACH BUG. YOU HAVE ONE CHANCE FOR A DIAGNOSIS TO LIVE. CHOOSE THE BEST SOLUTION.")
+
+    choice = input("Here is your diagnosis [1] Prochlorperazine or [2] Just drink water: ")
+
+    if choice == "1":
+        print("Thank you so much. THE PATIENT LEFT HAPPILY AND YOU STAYED ALIVE BECAUSE OF THE GREAT FEEDBACK SHE GAVE. CONGRATS YOU WIN!")
+        return "end"
+    else:
+        print("You did not listen to me at all. THE PATIENT LEFT ANGRILY AND LATER RETURNED DUE TO HER DISEASE INCREASING BECAUSE SHE REFUSED THE MEDICATION. YOU THEN GOT FIRED AND DIED THE SAME WEEK DUE TO YOUR FAILED JOB. YOU LOSE.")
+        return "end"
+
+
+current_room = "start"
+print("Welcome to doctor simulator")
+
+while current_room != "end":
+    if current_room == "start":
+        current_room = start_room()
+    elif current_room == "examine_room":
+        current_room = examine_room()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

083f4f0dcfa598a530b89dba7c0bc9ba4a980214
MaryamKhan13 <mishimk0116@gmail.com>
2026-04-30 19:36:53 +0000

    Data logger

---
+++ 013_Data_Logeer.py
@@ -0,0 +1,8 @@
+with open("attendance.txt","w") as file:
+ file.write("Haifa: Absent\n")\
+
+file.write("Janna:Present\n")
+file.write("Sir.Bryne Absent\n")
+file.write("Kiki Present\n")
+file.write("Layla Present\n")
+file.write("Coach Eyad Absent\n")

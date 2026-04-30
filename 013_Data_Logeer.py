3a7e79daa459220a33c6d4eef7c5c8f962b12e96
MaryamKhan13 <mishimk0116@gmail.com>
2026-04-30 19:45:28 +0000

    data

--- 013_Data_Logeer.py
+++ 013_Data_Logeer.py
@@ -1,8 +1,15 @@
 with open("attendance.txt","w") as file:
- file.write("Haifa: Absent\n")\
-
-file.write("Janna:Present\n")
-file.write("Sir.Bryne Absent\n")
-file.write("Kiki Present\n")
-file.write("Layla Present\n")
-file.write("Coach Eyad Absent\n")
+ file.write("Haifa: Absent\n")
+ file.write("Janna:Present\n")
+ file.write("Sir.Bryne: Absent\n")
+ file.write("Kiki: Present\n")
+ file.write("Layla: Present\n")
+ file.write("Coach Eyad: Absent\n")
+ 
+ with open("Attendance.txt","a") as file:
+  file.write("Daisy:Present\n")
+  print("-----Fetching Data From Disk----")
+  with open("attendance.txt","r") as file:
+   for line in file:
+    #.strip()removes the hidden '\n' at the end of the lines
+    print(line.strip())
